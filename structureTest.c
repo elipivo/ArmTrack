4aaf1a4cf539d3f84ddfa6d5aba27fd1b4dc0ee8
elipivo <elipivo@gmail.com>
2016-03-10 12:32:09 -0500

    Fixed a small thing.

--- structureTest.c
+++ structureTest.c
@@ -548,7 +548,7 @@ void* printSaveDataThread() {
 		}
 		//terminate data collection period
 		printf("\n");
-		fprintf(outFile, "\n");
+		fprintf(data.outFile, "\n");
 
 		pthread_mutex_unlock(&threadLocks[4]);
 
